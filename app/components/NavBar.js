aa9087454125191a17ee904c2d5f4a3e7cad5358
Chris Kang <christopha.kang@gmail.com>
2019-12-24 19:19:17 -0500

    adding popover menu

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logout } from '../redux'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Avatar, Menu } from '@material-ui/core'
 
 class NavBar extends React.Component {
   constructor() {
     super()
+    this.state = {anchorEl: null}
     this.handleLogout = this.handleLogout.bind(this)
+    this.handleClickAvatar = this.handleClickAvatar.bind(this)
+  }
+
+  handleClickAvatar(event) {
+    const anchorEl = this.state.anchorEl
+    this.setState({anchorEl: anchorEl ? null : event.currentTarget})
   }
 
   handleLogout() {
     this.props.logoutActionCreator(this.props.history)
   }
 
+  componentDidUpdate() {
+    console.log('state', this.state)
+  }
+
   render() {
     const { user, isLoggedIn } = this.props
+    const open = Boolean(this.state.anchorEl);
+    const id = open ? 'avatar-menu' : undefined;
+
     return (
       <div id="navbar-component">
         <AppBar position="static">
@@ -27,7 +41,23 @@ class NavBar extends React.Component {
               <div>
                 <NavLink to="/home">Home</NavLink>
                 <a href="#" onClick={this.handleLogout}>Logout</a>
-                <img src={user.imageUrl} style={{ width: '3rem' }} alt="User's profile photo." />
+                <Avatar src={user.imageUrl} style={{ width: '3rem' }} alt="User's profile photo." onClick={this.handleClickAvatar} />
+                <Menu
+                  id={id}
+                  open={open}
+                  anchorEl={this.state.anchorEl}
+                  onClose={this.handleClickAvatar}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                >
+                  Sample Menu Joy!!
+                </Menu>
               </div>
             ) : (
               <div>
