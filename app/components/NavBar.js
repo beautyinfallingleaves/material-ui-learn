a9cf71096fe864fdebb5922732d9294afd687452
Chris Kang <christopha.kang@gmail.com>
2019-12-24 23:11:30 -0500

    added logout menu

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,83 +1,28 @@
 import React from 'react'
-import { NavLink, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { logout } from '../redux'
-import { AppBar, Toolbar, Typography, Avatar, Menu } from '@material-ui/core'
+import { UserMenu } from './'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
 
-class NavBar extends React.Component {
-  constructor() {
-    super()
-    this.state = {anchorEl: null}
-    this.handleLogout = this.handleLogout.bind(this)
-    this.handleClickAvatar = this.handleClickAvatar.bind(this)
-  }
+const NavBar = (props) => {
+  const { user, isLoggedIn } = props
 
-  handleClickAvatar(event) {
-    const anchorEl = this.state.anchorEl
-    this.setState({anchorEl: anchorEl ? null : event.currentTarget})
-  }
-
-  handleLogout() {
-    this.props.logoutActionCreator(this.props.history)
-  }
-
-  componentDidUpdate() {
-    console.log('state', this.state)
-  }
-
-  render() {
-    const { user, isLoggedIn } = this.props
-    const open = Boolean(this.state.anchorEl);
-    const id = open ? 'avatar-menu' : undefined;
-
-    return (
-      <div id="navbar-component">
-        <AppBar position="static">
-          <Toolbar id="nav-content">
-            <Typography variant="h6" color="inherit">
-              Time Garden
-            </Typography>
-            {isLoggedIn ? (
-              <div>
-                <NavLink to="/home">Home</NavLink>
-                <a href="#" onClick={this.handleLogout}>Logout</a>
-                <Avatar src={user.imageUrl} style={{ width: '3rem' }} alt="User's profile photo." onClick={this.handleClickAvatar} />
-                <Menu
-                  id={id}
-                  open={open}
-                  anchorEl={this.state.anchorEl}
-                  onClose={this.handleClickAvatar}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                >
-                  Sample Menu Joy!!
-                </Menu>
-              </div>
-            ) : (
-              <div>
-                <NavLink to="/login">
-                  <Typography variant="body1">
-                    Log In
-                  </Typography>
-                </NavLink>
-                <NavLink to="/signup">
-                  <Typography variant="body1">
-                    Sign Up
-                  </Typography>
-                </NavLink>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </div>
-    )
-  }
+  return (
+    <div id="navbar-component">
+      <AppBar position="static">
+        <Toolbar id="nav-content">
+          <Typography variant="h6" color="inherit">
+            Time Garden
+          </Typography>
+          {isLoggedIn ? (
+            <UserMenu userImageUrl={user.imageUrl} />
+          ) : (
+            <React.Fragment />
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
@@ -87,10 +32,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    logoutActionCreator: (history) => dispatch(logout(history))
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
+export default withRouter(connect(mapStateToProps)(NavBar))
---
+++ app/components/UserMenu.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {logout} from '../redux'
+import {Avatar, ClickAwayListener, Grow, Paper, Popper, MenuItem, MenuList, makeStyles} from '@material-ui/core'
+import { withRouter } from 'react-router'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+}))
+
+const UserMenu = (props) => {
+  const { userImageUrl, history, logoutActionCreator } = props
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const anchorRef = React.useRef(null)
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen)
+  }
+
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault()
+      setOpen(false)
+    }
+  }
+
+  function handleLogout() {
+    logoutActionCreator(history)
+  }
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <Avatar
+          src={userImageUrl}
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        />
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem onClick={handleLogout}>Sign out</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logoutActionCreator: (history) => dispatch(logout(history))
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(UserMenu))
